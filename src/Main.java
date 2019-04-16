2e18d0880a643bdd3b46e40bcc714cbeee5555c0
bbishop19 <bbishop19@windwardschool.org>
2019-04-16 14:24:37 -0700

    swap dict.txt and add frequency ordering

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
-import java.io.IOException;
+
 import java.nio.file.Paths;
-import java.util.HashSet;
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-        SpellChecker spellChecker = new SpellChecker(Paths.get(Main.class.getClassLoader().getResource("dict.txt").getPath()), 500000);
-        System.out.println(spellChecker.dict.size());
+        SpellChecker spellChecker = new SpellChecker(Paths.get(Main.class.getClassLoader().getResource("dict.txt").getPath()), Paths.get(Main.class.getClassLoader().getResource("google-10000-english-usa.txt").getPath()),500000);
+        System.out.println(spellChecker.generateAndSortWords("solider"));
     }
 }
--- src/SpellChecker.java
+++ src/SpellChecker.java
@@ -2,12 +2,13 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashSet;
-import java.util.Scanner;
+import java.util.*;
 
 public class SpellChecker {
     public HashSet<String> dict;
-    public SpellChecker(Path dictionaryFile, int tableSize){//We do not take in a mispellingsFile because we generate the mispelling in run time
+    HashMap<String, Integer> wordFreq;
+
+    public SpellChecker(Path dictionaryFile, Path mostCommonWords, int tableSize){//We do not take in a mispellingsFile because we generate the mispelling in run time
         Scanner s = null;
         try{
             s = new Scanner(dictionaryFile);
@@ -19,6 +20,21 @@ public class SpellChecker {
         dict = new HashSet<>(tableSize);
         while(s.hasNext()){
             dict.add(s.next().toLowerCase());
+        }
+
+
+        try{
+            s = new Scanner(mostCommonWords);
+        }catch (IOException e){
+            e.printStackTrace();
+            System.exit(0);
+        }
+
+        wordFreq = new HashMap<>(11000);
+
+        int i = 0;
+        while(s.hasNext()){
+            wordFreq.put(s.next(), i++);
         }
         s.close();
     }
@@ -45,4 +61,24 @@ public class SpellChecker {
 
         return allPermutations;
     }
+
+    private PriorityQueue<String> sortPossibleWords(HashSet<String> possibleWords){
+        PriorityQueue pq = new PriorityQueue(wordFreqComparator);
+        for(String s : possibleWords){
+            pq.add(s);
+        }
+        return pq;
+    }
+
+    public PriorityQueue<String> generateAndSortWords(String s){
+        return sortPossibleWords(generatePossibleWords(s));
+    }
+
+    private Comparator<String> wordFreqComparator = new Comparator<String>() {
+        public int compare(String s1, String s2) {
+            int s1freq = wordFreq.containsKey(s1)?wordFreq.get(s1):10001;
+            int s2freq = wordFreq.containsKey(s2)?wordFreq.get(s2):10001;
+            return s1freq - s2freq;
+        }
+    };
 }
